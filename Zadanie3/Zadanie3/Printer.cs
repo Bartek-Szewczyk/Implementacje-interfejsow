08cb0d6059ad9a7dcb13c5a28eca6282cebcc884
Bartek-Szewczyk <ba20001000@gmail.com>
2020-11-22 20:32:46 +0100

    copier 12/12

---
+++ Zadanie3/Zadanie3/Printer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie3
+{
+    public class Printer : IPrinter
+    {
+        public static Printer CreateInstance()
+        {
+            return new Printer();
+        }
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+        
+        public void PowerOn()
+        {
+            state = IDevice.State.@on;
+        }
+
+        public void PowerOff()
+        {
+            state = IDevice.State.@off;
+        }
+
+
+
+        public int Counter { get; set; } =0;
+        public void Print(in IDocument doc1)
+        {
+            Console.WriteLine(DateTime.Now + " Print: " + doc1.GetFileName());
+            
+
+        }
+
+
+    }
+}
---
+++ Zadanie3/Zadanie3/Scanner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie3
+{
+    class Scanner : IScanner
+    {
+        public static Scanner CreateInstance()
+        {
+            return new Scanner();
+        }
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+
+        public void PowerOn()
+        {
+            state = IDevice.State.@on;
+        }
+
+        public void PowerOff()
+        {
+            state = IDevice.State.@off;
+        }
+
+
+        public int Counter { get; set; } = 0;
+        public void Scan(out IDocument doc1, IDocument.FormatType formatType)
+        {
+            doc1 = null;
+            switch (formatType)
+            {
+                case IDocument.FormatType.PDF:
+                    doc1 = new PDFDocument("PDFScan" + Counter + ".pdf");
+                    break;
+                case IDocument.FormatType.JPG:
+                    doc1 = new ImageDocument("ImageScan" + Counter + ".jpg");
+                    break;
+                case IDocument.FormatType.TXT:
+                    doc1 = new ImageDocument("TextScan" + Counter + ".txt");
+                    break;
+
+            }
+            Console.WriteLine(DateTime.Now + " Scan: " + doc1.GetFileName());
+           
+
+        }
+        public void Scan(out IDocument doc1)
+        {
+            doc1 = null;
+            doc1 = new PDFDocument("PDFScan" + Counter + ".pdf");
+            Console.WriteLine(DateTime.Now + " Scan: " + doc1.GetFileName());
+           
+
+        }
+    }
+}
